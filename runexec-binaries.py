78bed83a9d0ff8d72ab8b88600f0352dfe7dd414
Mohsen Ahmadvand <mohsen.ahmadvand@tum.de>
2017-11-16 14:50:02 +0000

    overhead measurement and result parsing

--- runexec-binaries.py
+++ runexec-binaries.py
@@ -13,41 +13,31 @@
 import os
 import json
 from pprint import pprint
-oh_results = {}
-sc_results = {}
+from subprocess import call
+
 def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
 
-def grab_results(result_directory):
-    oh_result = json.load(open(os.path.join(result_directory,"oh.stats")))
-   # pprint(oh_result)
-    sc_result = json.load(open(os.path.join(result_directory,"sc.stats")))
-   # pprint(sc_result)
-    #TODO: grab any other result file
-    return {"sc_result":sc_result,"oh_result":oh_result}
+def measure_overhead(result_directory,program):
+    program_path = os.path.join(result_directory,program)
+    #TODO run runexec 100 times and calculate avg and std
+    #call(["sosylib_measure.sh",program_path])
+    #TODO: run any other tool here
+    return {"run_result":{"cpu":1,"memory":1}}
 def process_results(results):
     #TODO do whatever and add outcome(s) to the results file
     print "process me"
     pprint(results)
     return results
 def process_files(directory):
-    program_results=[]
     for program_dir in get_immediate_subdirectories(directory):
-        coverage_results=[]
         for coverage_dir in get_immediate_subdirectories(os.path.join(directory,program_dir)):
-            combination_results =[]
             for combination_dir in get_immediate_subdirectories(os.path.join(directory,program_dir,coverage_dir)):
-                attempt_results=[]
                 attempt_path = os.path.join(directory,program_dir,coverage_dir,combination_dir);
                 for attempt_dir in get_immediate_subdirectories(attempt_path):
                     result_path = os.path.join(directory,program_dir,coverage_dir,combination_dir,attempt_dir);
-                    results = grab_results(result_path)
-                    attempt_results.append({"results":results, "attempt":attempt_dir})
-                combination_results.append({"combination":combination_dir,"attempt_results":attempt_results})
-            coverage_results.append({"coverage":coverage_dir, "combination_results":combination_results})
-            processed_coverage = process_results(coverage_results)
-        program_results.append({"program":program_dir,"coverage_results":coverage_results})
+                    results = measure_overhead(result_pathi,program_dir)
 
  #   pprint(program_results)
 
