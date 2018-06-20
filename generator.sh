0112bf1c4051d9a1e8d6a02157973d544d9163bb
Mohsen Ahmadvand <mohsen.ahmadvand@tum.de>
2018-06-20 14:06:46 +0000

    indent generator

--- generator.sh
+++ generator.sh
@@ -39,13 +39,13 @@ do
 	echo $bc
 	filename=${bc##*/}
 	libconfig=$config_path$filename
-        cmd_args=$(<$args_path$filename)
+	cmd_args=$(<$args_path$filename)
 	combination_dir=$combination_path$filename/*
 	libraries=$(<$link_libraries$filename)
-    patcher_args=$(<$patcher_scripts_path$filename)
-    patcher_args_array=($patcher_args)
-    response_file="${patcher_args_array[0]}"
-    gdb_script="${patcher_args_array[1]}"
+	patcher_args=$(<$patcher_scripts_path$filename)
+	patcher_args_array=($patcher_args)
+	response_file="${patcher_args_array[0]}"
+	gdb_script="${patcher_args_array[1]}"
 	echo "Libraries to link with $libraries"
 	for coverage_dir in $combination_dir
 	do
@@ -86,138 +86,138 @@ do
 			#repeat protection for random network of protection
 			for i in 1 2 3 
 			do
-			recover_attempt=0
-			while true;
-			do
-				output_dir=$binary_path$filename/$coverage_name/$combination_file/$i
-				mkdir -p $output_dir
-				echo "Protect here $i"
+				recover_attempt=0
+				while true;
+				do
+					output_dir=$binary_path$filename/$coverage_name/$combination_file/$i
+					mkdir -p $output_dir
+					echo "Protect here $i"
 
-				bitcode=$bc
-				#$output_dir/"guarded.bc"
-				 
-
-
-				echo 'Remove old files'
-				rm patch_guide
-				rm guide.txt
-				rm protected
-				rm out.bc
-				rm out 
-				echo 'Transform SC & OH'
-				opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $DG_PATH/libLLVMdg.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -use-cache -sc -extracted-only -use-other-functions -connectivity=1 -dump-checkers-network=$output_dir/"network_file" -dump-sc-stat=$output_dir/"sc.stats" -filter-file=$coverage -oh-insert -short-range-oh -num-hash 1 -dump-oh-stat=$output_dir/"oh.stats" -o $output_dir/out.bc >> $output_dir/transform.console 
-
-				echo $output_dir
-				if [ $? -eq 0 ]; then
-					echo 'OK Transform'
-					echo "opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $DG_PATH/libLLVMdg.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -use-cache -sc -extracted-only -use-other-functions -connectivity=1 -dump-checkers-network=$output_dir/"network_file" -dump-sc-stat=$output_dir/"sc.stats" -filter-file=$coverage -oh-insert -short-range-oh -num-hash 1 -dump-oh-stat=$output_dir/"oh.stats" -o $output_dir/out.bc >> $output_dir/transform.console"
-				else
-					echo !! 
-					echo 'FAIL Transform'
-					exit    
-				fi  
-				
-				
-				#link RTLIB 
-				llvm-link-3.9 $output_dir/out.bc $binary_path"rtlib.bc" -o $output_dir/out.bc
-				if [ $? -eq 0 ]; then
-					echo 'OK Link'
-				else
-					echo 'FAIL Link RTLib'
-					exit    
-				fi
-				# compiling external libraries to bitcodes
-				clang-3.9 $OH_PATH/assertions/response.c -c -fno-use-cxa-atexit -emit-llvm -o $OH_PATH/assertions/response.bc
-				if [ $? -eq 0 ]; then
-					echo 'OK '
-				else
-					echo 'FAIL Compile external'
-					exit    
-				fi  
-
-				 
-				echo 'Post patching binary after hash calls'
-				llc-3.9 $output_dir/out.bc -o $output_dir/out.s
-				if [ $? -eq 0 ]; then
-					echo 'OK Transform'
-				else
-					echo 'FAIL llc'
-					exit    
-				fi  
-				gcc -c -rdynamic $output_dir/out.s -o $output_dir/out.o $libraries
-				if [ $? -eq 0 ]; then
-					echo 'OK gcc -c'
-				else
-					echo 'FAIL gcc -c'
-					exit    
-				fi 
-				# Linking with external libraries
-		echo $response_file
-                if [ "$response_file" = "response.c" ]; then
-		    echo 'RUNNING GCC'
-                    gcc -g -rdynamic -c $OH_PATH/assertions/$response_file -o $output_dir/response.o
-                else
-		    echo 'RUNNING G++'
-                    g++ -std=c++0x -g -rdynamic -c $OH_PATH/assertions/$response_file -o $output_dir/response.o
-                fi
-				#gcc -g -rdynamic -c rtlib.c -o rtlib.o
-				if [ $? -eq 0 ]; then
-					echo 'OK gcc -g'
-				else
-					echo 'FAIL gcc -g'
-					exit    
-				fi 
-                if [ "$response_file" = "response.c" ]; then
-                    gcc -g -rdynamic $output_dir/out.o $output_dir/response.o -o $output_dir/$filename $libraries
-                else
-                    g++ -std=c++0x -g -rdynamic $output_dir/out.o $output_dir/response.o -o $output_dir/$filename $libraries
-                fi
-
-				if [ $? -eq 0 ]; then
-					echo 'OK gcc final binary'
-				else
-					echo "$link_libraries$filename"
-					echo "$libraries"
-					echo 'FAIL gcc final binary'
-					exit    
-				fi 
-				#remove temp files
-				rm $output_dir/out.o $output_dir/out.s $output_dir/response.o $output_dir/guarded.bc   
-				#clang++-3.9 -lncurses -rdynamic -std=c++0x out.bc -o out
-				python /home/sip/self-checksumming/patcher/dump_pipe.py $output_dir/$filename guide.txt patch_guide $output_dir/"sc.stats">> $output_dir/patcher.console
-				if [ $? -eq 0 ]; then
-					echo 'Done patching SC'
-				else
-					echo 'SC patcher failed'
-					exit
-				fi
+					bitcode=$bc
+					#$output_dir/"guarded.bc"
 
 
-				echo 'Starting GDB patcher, this will wait for input when nothing is provided'
-	
-                                echo $gdb_script
-				#Patch using GDB
-				echo "python $OH_PATH/patcher/patchAsserts.py -p $OH_PATH/assertions/$gdb_script -g "$cmd_args" -d True -b $output_dir/$filename -n $output_dir/$filename"tmp" -s $output_dir/"oh.stats" >> $output_dir/gdb.console"
-				timeout -k 9 10  python $OH_PATH/patcher/patchAsserts.py -p $OH_PATH/assertions/$gdb_script -g "$cmd_args" -d True -b $output_dir/$filename -n $output_dir/$filename"tmp" -s $output_dir/"oh.stats" >> $output_dir/"gdb.console" 
-				if [ $? -eq 0 ]; then
-					echo 'OK GDB Patch'
-					rm $output_dir/$filename
-					mv $output_dir/$filename"tmp" $output_dir/$filename
-					chmod +x $output_dir/$filename
-					recover_attempt=0
-					break # BREAK the while loop
-				else
-					echo "$? FAIL GDB Patch"
-					if [ $recover_attempt -eq 1 ]; then
-						echo "Failed to recover for three times" 
-						echo "Check $output_dir for more details"
-						break #exit 1
+
+					echo 'Remove old files'
+					rm patch_guide
+					rm guide.txt
+					rm protected
+					rm out.bc
+					rm out 
+					echo 'Transform SC & OH'
+					opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $DG_PATH/libLLVMdg.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -use-cache -sc -extracted-only -use-other-functions -connectivity=1 -dump-checkers-network=$output_dir/"network_file" -dump-sc-stat=$output_dir/"sc.stats" -filter-file=$coverage -oh-insert -short-range-oh -num-hash 1 -dump-oh-stat=$output_dir/"oh.stats" -o $output_dir/out.bc >> $output_dir/transform.console 
+
+					echo $output_dir
+					if [ $? -eq 0 ]; then
+						echo 'OK Transform'
+						echo "opt-3.9 -load $INPUT_DEP_PATH/libInputDependency.so -load $DG_PATH/libLLVMdg.so -load $UTILS_LIB -load $SC_PATH/libSCPass.so -load $OH_LIB/liboblivious-hashing.so -load $INPUT_DEP_PATH/libTransforms.so $bitcode -strip-debug -unreachableblockelim -globaldce -use-cache -sc -extracted-only -use-other-functions -connectivity=1 -dump-checkers-network=$output_dir/"network_file" -dump-sc-stat=$output_dir/"sc.stats" -filter-file=$coverage -oh-insert -short-range-oh -num-hash 1 -dump-oh-stat=$output_dir/"oh.stats" -o $output_dir/out.bc >> $output_dir/transform.console"
+					else
+						echo !! 
+						echo 'FAIL Transform'
+						exit    
+					fi  
+
+
+					#link RTLIB 
+					llvm-link-3.9 $output_dir/out.bc $binary_path"rtlib.bc" -o $output_dir/out.bc
+					if [ $? -eq 0 ]; then
+						echo 'OK Link'
+					else
+						echo 'FAIL Link RTLib'
+						exit    
 					fi
-					echo "trying to recover from Segmentation fault... $recover_attempt" >> $output_dir/segmentationfault.console
-					recover_attempt=$((recover_attempt+1))   
-				fi 
-			
-			done
+					# compiling external libraries to bitcodes
+					clang-3.9 $OH_PATH/assertions/response.c -c -fno-use-cxa-atexit -emit-llvm -o $OH_PATH/assertions/response.bc
+					if [ $? -eq 0 ]; then
+						echo 'OK '
+					else
+						echo 'FAIL Compile external'
+						exit    
+					fi  
+
+
+					echo 'Post patching binary after hash calls'
+					llc-3.9 $output_dir/out.bc -o $output_dir/out.s
+					if [ $? -eq 0 ]; then
+						echo 'OK Transform'
+					else
+						echo 'FAIL llc'
+						exit    
+					fi  
+					gcc -c -rdynamic $output_dir/out.s -o $output_dir/out.o $libraries
+					if [ $? -eq 0 ]; then
+						echo 'OK gcc -c'
+					else
+						echo 'FAIL gcc -c'
+						exit    
+					fi 
+					# Linking with external libraries
+					echo $response_file
+					if [ "$response_file" = "response.c" ]; then
+						echo 'RUNNING GCC'
+						gcc -g -rdynamic -c $OH_PATH/assertions/$response_file -o $output_dir/response.o
+					else
+						echo 'RUNNING G++'
+						g++ -std=c++0x -g -rdynamic -c $OH_PATH/assertions/$response_file -o $output_dir/response.o
+					fi
+					#gcc -g -rdynamic -c rtlib.c -o rtlib.o
+					if [ $? -eq 0 ]; then
+						echo 'OK gcc -g'
+					else
+						echo 'FAIL gcc -g'
+						exit    
+					fi 
+					if [ "$response_file" = "response.c" ]; then
+						gcc -g -rdynamic $output_dir/out.o $output_dir/response.o -o $output_dir/$filename $libraries
+					else
+						g++ -std=c++0x -g -rdynamic $output_dir/out.o $output_dir/response.o -o $output_dir/$filename $libraries
+					fi
+
+					if [ $? -eq 0 ]; then
+						echo 'OK gcc final binary'
+					else
+						echo "$link_libraries$filename"
+						echo "$libraries"
+						echo 'FAIL gcc final binary'
+						exit    
+					fi 
+					#remove temp files
+					rm $output_dir/out.o $output_dir/out.s $output_dir/response.o $output_dir/guarded.bc   
+					#clang++-3.9 -lncurses -rdynamic -std=c++0x out.bc -o out
+					python /home/sip/self-checksumming/patcher/dump_pipe.py $output_dir/$filename guide.txt patch_guide $output_dir/"sc.stats">> $output_dir/patcher.console
+					if [ $? -eq 0 ]; then
+						echo 'Done patching SC'
+					else
+						echo 'SC patcher failed'
+						exit
+					fi
+
+
+					echo 'Starting GDB patcher, this will wait for input when nothing is provided'
+
+					echo $gdb_script
+					#Patch using GDB
+					echo "python $OH_PATH/patcher/patchAsserts.py -p $OH_PATH/assertions/$gdb_script -g "$cmd_args" -d True -b $output_dir/$filename -n $output_dir/$filename"tmp" -s $output_dir/"oh.stats" >> $output_dir/gdb.console"
+					timeout -k 9 10  python $OH_PATH/patcher/patchAsserts.py -p $OH_PATH/assertions/$gdb_script -g "$cmd_args" -d True -b $output_dir/$filename -n $output_dir/$filename"tmp" -s $output_dir/"oh.stats" >> $output_dir/"gdb.console" 
+					if [ $? -eq 0 ]; then
+						echo 'OK GDB Patch'
+						rm $output_dir/$filename
+						mv $output_dir/$filename"tmp" $output_dir/$filename
+						chmod +x $output_dir/$filename
+						recover_attempt=0
+						break # BREAK the while loop
+					else
+						echo "$? FAIL GDB Patch"
+						if [ $recover_attempt -eq 1 ]; then
+							echo "Failed to recover for three times" 
+							echo "Check $output_dir for more details"
+							break #exit 1
+						fi
+						echo "trying to recover from Segmentation fault... $recover_attempt" >> $output_dir/segmentationfault.console
+						recover_attempt=$((recover_attempt+1))   
+					fi 
+
+				done
 			done
 		done
 	done
