1ca133399a063f36299650faa834e5f88e48795e
Mohsen Ahmadvand <mohsen.ahmadvand@tum.de>
2018-09-17 14:28:55 +0000

    fixed issues in scripts

--- run-all.sh
+++ run-all.sh
@@ -1,48 +1,85 @@
-#run clone and extraction passed to identify function names for the combination generator
+#run extraction passed to identify function names for the combination generator
 sh coverage-improver.sh 
 #extract filter fils (i.e. combinations) from the list of available function names
 sh combinator.sh
-#generat prottected binaries
+#generate SC+OH+SROH protected binaries
 bash generator.sh fdcontinue
 if [ $? -eq 0 ]; then
 
-	echo 'OK generator'
+	echo 'OK OH/SROH/SC generator'
 
 else
 
-	echo 'FAIL generator'
+	echo 'FAIL OH/SROH/SC generator'
+
+	exit    
+
+fi  
+#generate SC protected binaries
+bash generator-sc.sh fdcontinue
+if [ $? -eq 0 ]; then
+
+	echo 'OK SC generator'
+
+else
+
+	echo 'FAIL SC generator'
+
+	exit    
+
+fi 
+#run each protected program and measure the overhead imposed by the OH+SROH+SC protection
+sh runexec-binaries.sh binaries/
+if [ $? -eq 0 ]; then
+
+	echo 'OK OH/SROH/SC runexec'
+
+else
+
+	echo 'FAIL OH/SROH/SC runexec'
 
 	exit    
 
  fi  
-#run each protected program and measure the overhead imposed by the protection
-sh runexec-binaries.sh
+
+#run each protected program and measure the overhead imposed by the SC protection
+sh runexec-binaries.sh binaries-sc/
 if [ $? -eq 0 ]; then
 
-	echo 'OK runexec'
+        echo 'OK SC runexec'
+else
+        echo 'FAIL SC runexec'
+        exit
+ fi
+
+#extract measured cpu and memory overhead 
+python measure.py binaries/
+if [ $? -eq 0 ]; then
+
+	echo 'OK OH/SROH/SC measure'
 
 else
 
-	echo 'FAIL runexec'
+	echo 'FAIL OH/SROH/SC measure'
 
 	exit    
 
  fi  
 #extract measured cpu and memory overhead 
-python measure.py
+python measure.py binaries-sc/
 if [ $? -eq 0 ]; then
 
-	echo 'OK measure'
+        echo 'OK SC measure'
 
 else
 
-	echo 'FAIL measure'
+        echo 'FAIL SC measure'
 
-	exit    
+        exit
 
- fi  
+ fi
 #dump clone and extraction coverage improvement latex table
 #python measure-coverage-improvements.py
 #plot the extracted overhead data and dump protection coverage latex table
-python plot-dump.py
+python plot-dump.py -m binaries/measurements.json -p True
 
